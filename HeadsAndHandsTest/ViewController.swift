a835c59f722c9ce9f0eebc6e6a2b77c41b52f2fa
Roman Sentsov <zyoma108@gmail.com>
2019-01-16 09:46:19 +0500

    Initial Commit

---
+++ HeadsAndHandsTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HeadsAndHandsTest
+//
+//  Created by Roman Sentsov on 16/01/2019.
+//  Copyright © 2019 Roman Sentsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
