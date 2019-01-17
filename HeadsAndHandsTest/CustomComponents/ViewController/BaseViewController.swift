6c49291447c53356d1f3e0e38b111e14e51dad78
Roman Sentsov <zyoma108@gmail.com>
2019-01-17 10:37:05 +0500

    Improve BaseViewController

--- HeadsAndHandsTest/CustomComponents/ViewController/BaseViewController.swift
+++ HeadsAndHandsTest/CustomComponents/ViewController/BaseViewController.swift
@@ -11,6 +11,35 @@ import RxSwift
 
 class BaseViewController: UIViewController {
     
+    // MARK: - Properties
     let disposeBag = DisposeBag()
+    
+    // MARK: - Life Cycle
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        unsubscribeForKeyboardEvents()
+    }
 
+    // MARK: - Keyboard
+    func unsubscribeForKeyboardEvents() {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    func subscribeForKeyboardEvents() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillAppear), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillDisappear), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc private func keyboardWillAppear(notification: NSNotification) {
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardHeight = keyboardFrame.cgRectValue.height
+            keyboardWillAppearWithHeight(keyboardHeight)
+        }
+    }
+    
+    @objc func keyboardWillDisappear() { }
+    
+    func keyboardWillAppearWithHeight(_ keyboardHeight: CGFloat) { }
+    
 }
