31679cb748daab11134f128f5e5e4526e37fd321
Roman Sentsov <zyoma108@gmail.com>
2019-01-17 10:04:49 +0500

    Add empty LoginViewController

---
+++ HeadsAndHandsTest/Scenes/Login/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  HeadsAndHandsTest
+//
+//  Created by Roman Sentsov on 17/01/2019.
+//  Copyright © 2019 Roman Sentsov. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: BaseViewController {
+    
+    // MARK: - Properties
+    let viewModel = LoginViewModel()
+
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ HeadsAndHandsTest/Scenes/Login/LoginViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewModel.swift
+//  HeadsAndHandsTest
+//
+//  Created by Roman Sentsov on 17/01/2019.
+//  Copyright © 2019 Roman Sentsov. All rights reserved.
+//
+
+import Foundation
+
+class LoginViewModel {
+    
+}
